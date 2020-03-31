00ffca3604126802b2fe1928c22e1b7dd13698dc
Maksym <maksimbukhalo@gmail.com>
2020-03-31 14:18:51 +0300

    Lesson3 1.0.0
    Complite Example of using Unity Addressable Asset System

---
+++ Assets/Scripts/AddressableAssetSystem/ControllerCubes.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.UI;
+
+public class ControllerCubes : MonoBehaviour
+{
+    [SerializeField] private Button instantiateCubeButtton;
+
+    [SerializeField] private Button destroyCubeButton;
+
+    [SerializeField] private Button destroyAllCubeButton;
+
+    private List<GameObject> cubes = new List<GameObject>(); 
+
+    private void Awake()
+    {
+        instantiateCubeButtton.onClick.AddListener(InstantiateCube);
+        destroyCubeButton.onClick.AddListener(DestroyCube);
+        destroyAllCubeButton.onClick.AddListener(DestroyAllCube);
+    }
+
+    private void InstantiateCube()
+    {
+        var position = cubes.Count * 1.25f * Vector3.forward;
+        Addressables.InstantiateAsync("Cube", position, Quaternion.identity).Completed += handle =>
+        {
+            cubes.Add(handle.Result);
+        };
+    }
+    private void DestroyCube()
+    {
+        if (cubes.Count<=0)
+        {
+            return;
+        }
+
+        var lastCube = cubes.Last();
+        Addressables.ReleaseInstance(lastCube);
+        cubes.Remove(lastCube);
+    }
+
+
+    private void DestroyAllCube()
+    {
+        foreach(var cube in cubes)
+        {
+            Addressables.ReleaseInstance(cube);
+        }
+
+        cubes.Clear();
+    }
+
+}
